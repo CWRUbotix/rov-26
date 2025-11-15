90ba3865fc8ed9cdd84bff787c88a4b1a2dece22
Fotih Turopov <fotihturopov@gmail.com>
2025-11-15 14:57:15 -0500

    Refactor MillisecondTimerWidget to use QWidget

--- src/surface/gui/gui/widgets/millisecond_timer.py
+++ src/surface/gui/gui/widgets/millisecond_timer.py
@@ -1,23 +1,34 @@
+from PyQt6.QtCore import Qt, QTimer, pyqtSlot
 from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QLabel, QVBoxLayout, QWidget
 
-
-class MillisecondTimerWidget(QLabel):
-    def __init__(self) -> None:
+class MillisecondTimerWidget(QWidget):
+    def __init__(self):
         super().__init__()
 
-        timer_layout = QVBoxLayout()
-
+        layout = QVBoxLayout()
         font = QFont('Arial', 14)
 
         self.timer_label = QLabel('00:00:000')
         self.timer_label.setFont(font)
-        timer_layout.addWidget(self.timer_label)
+        self.timer_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.setLayout(timer_layout)
+        layout.addWidget(self.timer_label)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+        self._elapsed_ms: int = 0
+        self._timer = QTimer(self)
+        self._timer.setInterval(10)
+        self._timer.timeout.connect(self._on_timeout)
+        self._timer.start()        
+
+    def _on_timeout(self) -> None:
+        self._elapsed_ms += self._timer.interval()
+        self.update_time(self._elapsed_ms)
 
     def update_time(self, milliseconds: int) -> None:
         minutes = milliseconds // 60000
         seconds = (milliseconds % 60000) // 1000
         millis = milliseconds % 1000
-        self.setText(f'{minutes:02}:{seconds:02}:{millis:03}')
+        self.timer_label.setText(f'{minutes:02}:{seconds:02}:{millis:03}')
